6863fa3424ccbb12c19f1df13f2a9c3139bd8dd5
junsung-jeon <jackjjs73@gmail.com>
2026-03-11 13:03:30 +0900

    fix: GitHub Pages 배포 설정 수정

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import './styles/global.css';
 export default function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/weniv-blog">
         <Header />
         <Routes>
           <Route path="/"          element={<HomePage />} />
